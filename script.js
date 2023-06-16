5dbeab7cc1ab36bd0cf7eceb61ff4d4c5d313dd2
Lucas Haas Cordeiro <luc.cordeiro@gmail.com>
2023-06-16 11:01:16 -0300

    new topbar

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 // toggle submenu
-
 function toggleSubmenu(event, submenuId) {
     event.preventDefault();
     const submenu = document.getElementById(submenuId);
+
+
+// Hide all other submenus before showing the selected one
+const submenus = document.getElementsByClassName('submenu');
+for (let i = 0; i < submenus.length; i++) {
+  const currentSubmenu = submenus[i];
+  if (currentSubmenu.id !== submenuId) {
+    currentSubmenu.classList.remove('show');
+  }
+}
+
     submenu.classList.toggle('show');
   }
